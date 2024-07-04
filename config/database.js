d4a45a5d37c51d5cbad05f4ba8c71a546aa51993
KINGBOTs1 <ds767365@gmail.com>
2024-07-05 03:21:18 +0800

    new

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const db = new Sequelize({
     database: 'gisManggelewa',
     username: 'gisManggelewa',
     password: 'gisManggelewa',
-    host: '127.0.0.1',
+    host: '35.240.249.138',
     port: 3306,
     dialect: "mysql",
 });
